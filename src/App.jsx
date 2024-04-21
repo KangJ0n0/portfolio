9aa39af9b98e5a62197c418591ee4f6dbb36e06c
Jono <127705274+KangJ0n0@users.noreply.github.com>
2024-04-21 14:57:36 +0700

    for now+

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import Test from "./Test";
 import "./app.scss";
 import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Portfolio from "./components/portfolio/Portfolio";
-import About from "./components/About/About";
+import About from "./components/about/About";
 
 const App = () => {
   return (
